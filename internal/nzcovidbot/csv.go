87a0cf7158d1587ec834eb076fe7b3ecd0390acb
Daniel Mason <daniel@m2.nz>
2021-10-04 08:26:59 +1300

    Bump to 60 limit

--- internal/nzcovidbot/csv.go
+++ internal/nzcovidbot/csv.go
@@ -64,8 +64,8 @@ func parseCsvRow(data string) {
 		updatedLocations.Locations = append(updatedLocations.Locations, newRow)
 	}
 
-	if len(updatedLocations.Locations) > 40 {
-		log.Print("Not displaying > 40 locations!")
+	if len(updatedLocations.Locations) > 60 {
+		log.Print("Not displaying > 60 locations!")
 		updatedLocations = UpdatedLocations{}
 	}
 }
